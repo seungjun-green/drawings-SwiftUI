f2e8c6b5fa215edadea67718131180ce649a2852
seungjun-green <lsj3285007@gmail.com>
2022-07-05 01:22:26 +0900

    Initial Commit

---
+++ drawings-SwiftUI/drawings_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawings_SwiftUIApp.swift
+//  drawings-SwiftUI
+//
+//  Created by SeungJun Lee on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct drawings_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

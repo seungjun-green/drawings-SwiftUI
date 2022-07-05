16fc7bb03af48488a0dde0cbe10785968d7a8716
seungjun-green <lsj3285007@gmail.com>
2022-07-05 20:58:02 +0900

    added google logo(N)

---
+++ drawings-SwiftUI/GoogleLogo/GoogleColors.swift
@@ -0,0 +1,31 @@
+//
+//  GoogleColors.swift
+//  drawings-SwiftUI
+//
+//  Created by SeungJun Lee on 7/5/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    
+
+    static var googleBlue: Color {
+        Color(red: 66/255, green: 133/255, blue: 244/255)
+    }
+    
+    
+    static var googleGreen: Color {
+        Color(red: 52/255, green: 168/255, blue: 83/255)
+    }
+    
+    static var googleYellow: Color {
+        Color(red: 251/255, green: 188/255, blue: 5/255)
+    }
+    
+    static var googleRed: Color {
+        Color(red: 234/255, green: 67/255, blue: 53/255)
+    }
+    
+}
